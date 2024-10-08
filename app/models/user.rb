11d991fbbb8e5a6129f5a34b2a50d09293819a0d
lukblan <luk.blanko@gmail.com>
2024-10-08 09:59:28 -0300

    Add implementation of User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ApplicationRecord
+  attr_reader :password
+  validates :username, presence: true, uniqueness: true
+  before_validation :create_session_token
+
+  def self.find_by_session_token(session_token)
+    self.find_by(session_token: session_token)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username)
+
+    return nil unless user
+    return nil unless user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    self.password_digest ||= BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def create_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
